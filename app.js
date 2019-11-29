4c35949d0530c92db1f6b7cd50e7a05ee78028de
Adityesh <adityeshmishra98@gmail.com>
2019-11-29 20:08:53 +0530

    Working app

--- app.js
+++ app.js
@@ -1,13 +1,41 @@
-var querySearch = document.getElementById("search-query").value;
-var searchBtn = document.querySelector('.btn-search');
-
-
-searchBtn.addEventListener('click',()=>{
-    var api = `https://en.wikipedia.org/w/api.php?action=opensearch&search=${querySearch}&limit=10&namespace=0&format=json`
-    fetch(api,{mode: 'cors',})
-        .then(response => response.json())
-        .then(data =>console.log(data))
-})
-
-
-
+//Run the function when the scripts are fully loaded
+$( document ).ready(function() {
+    //input value from user
+    var searchTerm = $("input[type=text]").val();
+    //Api url to connect to wikipedia
+    var api = `https://en.wikipedia.org/w/api.php?format=json&action=query&generator=search&gsrnamespace=0&gsrlimit=10&prop=pageimages|extracts&pilimit=max&exintro&explaintext&exsentences=1&exlimit=max&gsrsearch=`;
+    var cb = '&callback=?'; 
+    //Search button click
+    $("#searchBtn").click(function(){
+        var searchTerm = $("input[type=text]").val();
+        var api = `https://en.wikipedia.org/w/api.php?format=json&action=query&generator=search&gsrnamespace=0&gsrlimit=10&prop=pageimages|extracts&pilimit=max&exintro&explaintext&exsentences=1&exlimit=max&gsrsearch=`;
+        var cb = '&callback=?'; 
+        //Search field empty then clear all results child nodes
+        if(searchTerm == ''){
+            $('.article-box').empty();
+        }
+        $(".article-box").empty();
+        //Api call and store data in results title and extracts array
+            $.getJSON(api + searchTerm + cb, function(data){
+                 var results = data.query.pages; 
+                 var titles = [];
+                 var extracts = [];
+                 var pages = [];
+                for(let key in results){
+                    extracts.push(results[key].extract);
+                    titles.push(results[key].title);
+                    pages.push(results[key].pageid);
+                }
+                //iterate and append to results class in html
+                for(let i =0 ;i<titles.length;i++){
+                    
+                    $(".article-box").append(function(){   
+                        return `<div class="articles">
+                        <h3><a href="https://en.wikipedia.org/?curid=+${pages[i]}" target="_blank">${titles[i]}</a></h3>
+                        <p>${extracts[i]}</p>
+                        </div>`
+                    })   
+                }    
+            });     
+    });   
+});
